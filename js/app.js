5d7d366e93efb646242606da9fea5035830be4c7
Sen2017 <casintahan47at@gmail.com>
2016-12-14 09:22:15 -1000

    confused

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log('Sanity Check');
+
+function color1() {
+	
+	console.log("color_change_NUKE_arm");
+	document.getElementById("square").style.backgroundColor = "orange";
+
+}
+
+function color2() {
+
+	console.log("color_change_NUKE_detonate");
+	document.getElementById("square").style.backgroundColor = "red";
+
+}
+
+function color3() {
+
+	console.log("color_change_NUKE_disarm");
+	document.getElementById("square").style.backgroundColor = "gray";
+
+}
