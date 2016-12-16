2b472492f78b268657c32b29d33573f3359ec45f
Sen2017 <casintahan47at@gmail.com>
2016-12-16 09:04:53 -1000

    cinnamon and bobo added

--- js/app.js
+++ js/app.js
@@ -2,21 +2,37 @@ console.log('Sanity Check');
 
 function color1() {
 	
-	console.log("color_change_NUKE_arm");
-	document.getElementById("square").style.backgroundColor = "orange";
+	console.log("cinnamon_is_blushing");
+	var changeDiv = document.getElementById("cinnamon");
+	changeDiv.style.backgroundColor = "pink";
+	changeDiv.innerHTML = "huehuehue ^-^ STAHP";
 
 }
 
 function color2() {
 
-	console.log("color_change_NUKE_detonate");
-	document.getElementById("square").style.backgroundColor = "red";
+	console.log("cinnamon_is_not_blushing");
+	var changeDiv = document.getElementById("cinnamon");
+	changeDiv.style.backgroundColor = "tan";
+	changeDiv.innerHTML = "hi, i'm cinnamon. tickles make me blush";
 
 }
 
 function color3() {
 
-	console.log("color_change_NUKE_disarm");
-	document.getElementById("square").style.backgroundColor = "gray";
+	console.log("bobo_is_angry");
+	var changeDiv = document.getElementById("bobo");
+	changeDiv.style.backgroundColor = "red";
+	changeDiv.innerHTML = "WHAT DID I SAY?!";
+
+
+}
+
+function color4() {
+
+	console.log("bobo_is_not_angry")
+	var changeDiv = document.getElementById("bobo");
+	changeDiv.style.backgroundColor = "tan";
+	changeDiv.innerHTML = "i'm bobo... i said don't touch me >.>"
 
 }
